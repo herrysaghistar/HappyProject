08af38fbf1520211b94776aeb745067144b6e58e
herrysaghistar <hepi321321321@gmail.com>
2024-05-19 22:10:50 +0700

    redirect login

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -26,6 +26,28 @@ class LoginController extends Controller
      * @var string
      */
     protected $redirectTo = '/home';
+    // protected function redirectTo()
+    // {
+    //     if (Auth::check()) {
+    //         $user = Auth::user();
+            
+    //         if ($user->hasRole('admin')) {
+    //             return '/admin';
+    //         } elseif ($user->hasRole('hse')) {
+    //             return '/hse';
+    //         } elseif ($user->hasRole('kabeng')) {
+    //             return '/kabeng';
+    //         } elseif ($user->hasRole('kapro')) {
+    //             return '/kapro';
+    //         } elseif ($user->hasRole('spv')) {
+    //             return '/spv';
+    //         } elseif ($user->hasRole('karyawan')) {
+    //             return '/karyawan';
+    //         }
+    //     }
+
+    //     return '/home'; // Default redirect
+    // }
 
     /**
      * Create a new controller instance.
