b435202baeaf84377af5917158f8de3a7acb3594
herrysaghistar <hepi321321321@gmail.com>
2024-05-19 21:58:53 +0700

    templating and grouping

--- routes/web.php
+++ routes/web.php
@@ -20,26 +20,26 @@ Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 // Define routes with middleware for each permission
-Route::middleware('permission:admin')->group(function () {
+Route::middleware('can:admin')->group(function () {
     Route::get('/admin', [AdminController::class, 'index']);
 });
 
-Route::middleware('permission:hse')->group(function () {
+Route::middleware('can:hse')->group(function () {
     Route::get('/hse', [HseController::class, 'index']);
 });
 
-Route::middleware('permission:kabeng')->group(function () {
+Route::middleware('can:kabeng')->group(function () {
     Route::get('/kabeng', [KabengController::class, 'index']);
 });
 
-Route::middleware('permission:kapro')->group(function () {
+Route::middleware('can:kapro')->group(function () {
     Route::get('/kapro', [KaproController::class, 'index']);
 });
 
-Route::middleware('permission:spv')->group(function () {
+Route::middleware('can:spv')->group(function () {
     Route::get('/spv', [SpvController::class, 'index']);
 });
 
-Route::middleware('permission:karyawan')->group(function () {
+Route::middleware('can:karyawan')->group(function () {
     Route::get('/karyawan', [KaryawanController::class, 'index']);
 });
