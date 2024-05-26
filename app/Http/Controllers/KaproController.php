78bf09efdec4bfce192856ac459351d22ab21afb
herrysaghistar <hepi321321321@gmail.com>
2024-05-26 18:26:05 +0700

    pelengkapan

--- app/Http/Controllers/KaproController.php
+++ app/Http/Controllers/KaproController.php
@@ -12,15 +12,34 @@ class KaproController extends Controller
 {
     public function index()
     {
-    	$data = ptw::join('projects', 'ptws.project_id', '=', 'projects.id')
+        $data = Ptw::select(
+                        'ptws.id as ptw_id',
+                        'ptws.*',
+                        'projects.*',
+                        'work_locations.*',
+                        'permission_types.*',
+                        DB::raw('GROUP_CONCAT(DISTINCT permission_tambahans.permission_name) as permission_names'),
+                        DB::raw('GROUP_CONCAT(DISTINCT tools_types.tools_name) as tools_names')
+                    )
+                    ->join('projects', 'ptws.project_id', '=', 'projects.id')
                     ->join('work_locations', 'ptws.work_location_id', '=', 'work_locations.id')
-                    ->select('ptws.id as ptw_id' ,'ptws.*',  'projects.*', 'work_locations.*')
+                    ->join('permission_types', 'ptws.permission_id', '=', 'permission_types.id')
+                    
+                    ->leftJoin('ptw_permissions', 'ptws.id', '=', 'ptw_permissions.ptw_id')
+                    ->leftJoin('permission_tambahans', 'ptw_permissions.permission_id', '=', 'permission_tambahans.id')
+                    
+                    ->leftJoin('ptw_tools', 'ptws.id', '=', 'ptw_tools.ptw_id')
+                    ->leftJoin('tools_types', 'ptw_tools.tools_id', '=', 'tools_types.id')
                     ->whereIn('level', ['kapro', 'approved', 'rejected'])
-    				->get();
-    	$project = project::all();
+                    ->groupBy('ptws.id')
+                    ->get();
+
+        $tools = tools_type::all();
+        $permission_tambahan = permission_tambahan::all();
+        $project = project::all();
         $work_location = work_location::all();
         $permission_type = permission_type::all();
 
-        return view('permission.permission', compact('data', 'project', 'work_location', 'permission_type'));
+        return view('permission.permission', compact('data', 'project', 'work_location', 'permission_type', 'permission_tambahan', 'tools'));
     }
 }
