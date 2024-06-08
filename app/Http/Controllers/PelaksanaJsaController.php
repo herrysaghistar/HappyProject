e3e952098905da059962beb41a049292fc037cad
herrysaghistar <hepi321321321@gmail.com>
2024-06-08 11:00:13 +0700

    a

---
+++ app/Http/Controllers/PelaksanaJsaController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\pelaksana_jsa;
+use App\Http\Requests\Storepelaksana_jsaRequest;
+use App\Http\Requests\Updatepelaksana_jsaRequest;
+
+class PelaksanaJsaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Storepelaksana_jsaRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(pelaksana_jsa $pelaksana_jsa)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(pelaksana_jsa $pelaksana_jsa)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Updatepelaksana_jsaRequest $request, pelaksana_jsa $pelaksana_jsa)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(pelaksana_jsa $pelaksana_jsa)
+    {
+        //
+    }
+}
